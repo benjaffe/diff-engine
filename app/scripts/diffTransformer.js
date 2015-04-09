05916f5609a662dabb1c3cacd3ea9078cd7c9a78
Ben Jaffe <ben@udacity.com>
2015-04-09 11:46:49 -0700

    cleanup

--- app/scripts/diffTransformer.js
+++ app/scripts/diffTransformer.js
@@ -2,163 +2,177 @@
 
 var app = app || {};
 
-app.diffEngine = (function(){
+app.diffEngine = (function() {
 
-	var _diffArr = [];
-	var _prevContent;
-	var _prevDate;
+  var _diffArr = [];
+  var _prevContent;
+  var _prevDate;
 
-	var _history = [];
+  var _history = [];
 	
-	var _diffAndHistoryOutOfSync = true;
+  var _diffAndHistoryOutOfSync = true;
 
-	var encoder = {
-		push: function(content) {
-			var diff = _generateDiff(content);
-			_diffArr.push(diff);
-			_diffAndHistoryOutOfSync = true;
-		}
-	};
+  var encoder = {
+  	/**
+  	 * Calculates the diff from the content and adds it to the diff list
+  	 * @param  {string} content - the content to be diffed
+  	 */
+    push: function(content) {
+      var diff = _generateDiff(content);
+      _diffArr.push(diff);
+      _diffAndHistoryOutOfSync = true;
+    }
+  };
 
-	var decoder = {
-		getDiffs: function() {
-			return _diffArr;
-		},
+  var decoder = {
+    /**
+		 * Get the diff array
+		 * @return {array} An array of diff objects
+		 */
+    getDiffs: function() {
+      return _diffArr;
+    },
 
-		/**
+    /**
 		 * Return the final state of the content
-		 * @return {[type]} [description]
+		 * @return {object}  The state object after the diffs are decoded into content
 		 */
-		getContent: function() {
-			return _decodeContent();
-		},
+    getContent: function() {
+      return _decodeContent();
+    },
 		
-		/**
-		 * Return the state of the content at dif # num
-		 * @param  {[type]} num [diff number]
-		 * @return {[type]}     [description]
+    /**
+		 * Get the state of the content at dif # num
+		 * @param  {integer} num - the number of the diff to be decoded
+		 * @return {string}        the decoded content
 		 */
-		getContentAtIndex: function(num) {
-			if (num > _diffArr.length - 1) {
-				return null;
-			}
+    getContentAtIndex: function(num) {
+      if (num > _diffArr.length - 1) {
+        return null;
+      }
 
-			return _decodeContent(num);
-		},
+      return _decodeContent(num);
+    },
 
-		getContentAtTime: function(time) {
+    /**
+		 * Get the state of the content at a given time
+		 * @param  {integer} time - ms from start to be decoded
+		 * @return {string}        the decoded content
+		 */
+    getContentAtTime: function(time) {
 
-		},
+    },
 
-		getContentTimeline: function() {
-			if (_diffArr.length !== _history.length) {
-				_decodeContent();
-			}
-			return _history;
-		}
-	};
+    /**
+     * Get a timeline of decoded content
+     * @return {array} timeline of decoded content
+     */
+    getContentTimeline: function() {
+      if (_diffArr.length !== _history.length) {
+        _decodeContent();
+      }
+      return _history;
+    }
+  };
 
-	function _generateDiff (content) {
-		var _diff = {};
-		var _now = Date.now();
+  function _generateDiff (content) {
+    var _diff = {};
+    var _now = Date.now();
 
-		if (!_prevContent) {
-			_diff.timeAbsolute = _now;
-		} else {
-			_diff.timeRelative = _now - _prevDate;
-		}
+    if (!_prevContent) {
+      _diff.timeAbsolute = _now;
+    } else {
+      _diff.timeRelative = _now - _prevDate;
+    }
 
-		if (content) {
-			_diff.content = app.JsDiff.diffChars(_prevContent, content);
-		}
+    if (content) {
+      _diff.content = app.JsDiff.diffChars(_prevContent, content);
+    }
 
-		_prevContent = content;
-		_prevDate = _now;
+    _prevContent = content;
+    _prevDate = _now;
 			
-		return _diff;
-	}
+    return _diff;
+  }
 
-	// TODO: decodes based on the diff index
-	// this is temporary for initial testing purposes
+  // TODO: decodes based on the diff index
+  // this is temporary for initial testing purposes
 	
-	/**
-	 * [_decodeContent description]
-	 * @param  {[type]} _num [description]
-	 * @return {[type]}      [description]
-	 */
-	function _decodeContent (_num) {
+  /**
+  	 * [_decodeContent description]
+  	 * @param  {[type]} _num [description]
+  	 * @return {[type]}      [description]
+  	 */
+  function _decodeContent (_num) {
 		
-		if (_num === undefined) {
-			_num = _diffArr.length - 1;
-		}
+    if (_num === undefined) {
+      _num = _diffArr.length - 1;
+    }
 
-		if (_diffArr.length === _history.length && !_diffAndHistoryOutOfSync) {
-			return _history[_num];
-		}
+    if (_diffArr.length === _history.length && !_diffAndHistoryOutOfSync) {
+      return _history[_num];
+    }
 
-		// reset history, calculate everything from scratch
-		_history = [];
+    // reset history, calculate everything from scratch
+    _history = [];
 
-		console.log('decoding');
+    console.log('decoding');
 		
-		// go through the diff array
-		_diffArr.forEach(function(diff, diffNum){
-			var i = 0; // our virtual cursor
-			var str = _history[diffNum - 1] && _history[diffNum - 1].content || ''; // last string value or ''
-			var timestampAbs, timestamp;
+    // go through the diff array
+    _diffArr.forEach(function(diff, diffNum) {
+      var timestampAbs, timestamp;
 
+      // our virtual cursor
+      var i = 0;
 
-			// run through each diff and calculate contents
-			diff.content.forEach(function(diffOp){
-				if (diffOp.added) {
-					str = str.substr(0, i) + diffOp.value + str.slice(i /*-1+diffOp.value.length*/ );
-					i += diffOp.value.length;
-				}
+      // last string value or ''
+      var str = _history[diffNum - 1] && _history[diffNum - 1].content || '';
 
-				else if (diffOp.removed) {
-					str = str.substr(0, i) + str.slice(i+diffOp.count);
-				}
+      // run through each diff and calculate contents
+      diff.content.forEach(function(diffOp) {
+        if (diffOp.added) {
+          str = str.substr(0, i) + diffOp.value + str.slice(i);
+          i += diffOp.value.length;
+        } else if (diffOp.removed) {
+          str = str.substr(0, i) + str.slice(i + diffOp.count);
+        } else {
+          i += diffOp.count;
+        }
+      });
 
-				else {
-					i += diffOp.count;
-				}
-			});
+      // set the timestamps
+      if (diff.timeAbsolute) {
+        timestampAbs = diff.timeAbsolute;
+        timestamp = 0;
+      } else {
+        timestampAbs = _history[diffNum - 1].timestampAbs + diff.timeRelative;
+        timestamp = _history[diffNum - 1].timestamp + diff.timeRelative;
+      }
 
-
-			// set the timestamps
-			if (diff.timeAbsolute) {
-				timestampAbs = diff.timeAbsolute;
-				timestamp = 0;
-			} else {
-				timestampAbs = _history[diffNum - 1].timestampAbs + diff.timeRelative;
-				timestamp = _history[diffNum - 1].timestamp + diff.timeRelative;
-			}
-
-
-			_history.push({
-				content: str,
-				timestamp: timestamp,
-				timestampAbs: timestampAbs,
-				index: _history.length
-			});
-		});
+      _history.push({
+        content: str,
+        timestamp: timestamp,
+        timestampAbs: timestampAbs,
+        index: _history.length
+      });
+    });
 		
-		_diffAndHistoryOutOfSync = false;
+    _diffAndHistoryOutOfSync = false;
 
-		return _history[_num];
-	}
+    return _history[_num];
+  }
 
-	function getEncoder() {
-		return encoder;
-	}
+  function getEncoder() {
+    return encoder;
+  }
 
-	function getDecoder() {
-		return decoder;
-	}
+  function getDecoder() {
+    return decoder;
+  }
 
-	return {
-		getEncoder: getEncoder,
-		getDecoder: getDecoder
-	};
+  return {
+    getEncoder: getEncoder,
+    getDecoder: getDecoder
+  };
 
 })();
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,62 +1,63 @@
 /* global describe, it */
 
 (function () {
-	'use strict';
+  'use strict';
 
-	String.prototype.splice = function( idx, rem, s ) {
-		return (this.slice(0,idx) + (s||'') + this.slice(idx + Math.abs(rem)));
-	};
+  String.prototype.splice = function(idx, rem, s) {
+    return (this.slice(0, idx) + (s || '') + this.slice(idx + Math.abs(rem)));
+  };
 
-	describe('Diff APIs', function () {
+  describe('Diff APIs', function () {
 		
-		describe('should be symmetrical', function () {
+    describe('should be symmetrical', function () {
 
-			var contentEncoder = app.diffEngine.getEncoder();
-			var contentDecoder = app.diffEngine.getDecoder();
-			var str = '<TextView\n    android:text=\"Hi there!\"\n    android:padding=\"20dp\"/>';
+      var contentEncoder = app.diffEngine.getEncoder();
+      var contentDecoder = app.diffEngine.getDecoder();
+      var str = '<TextView\n    android:text=\"Hi there!\"\n    android:padding=\"20dp\"/>';
 
-			it('for adding the initial string', function () {
-				var decodedContent;
-				contentEncoder.push(str);
-				decodedContent = contentDecoder.getContent().content;
-				decodedContent.should.equal(str);
-			});
+      it('for adding the initial string', function () {
+        var decodedContent;
+        contentEncoder.push(str);
+        decodedContent = contentDecoder.getContent().content;
+        decodedContent.should.equal(str);
+      });
 
-			it('when adding text to the end', function () {
-				str = str + ' ... that\'s all folks';
-				var decodedContent;
-				contentEncoder.push(str);
-				decodedContent = contentDecoder.getContent().content;
-				decodedContent.should.equal(str);
-			});
+      it('when adding text to the end', function () {
+        str = str + ' ... that\'s all folks';
+        var decodedContent;
+        contentEncoder.push(str);
+        decodedContent = contentDecoder.getContent().content;
+        decodedContent.should.equal(str);
+      });
 
-			it('when adding text to the middle', function () {
-				str = str.splice(28, 0, 'Oh, here we are... ');
-				var decodedContent;
-				contentEncoder.push(str);
-				decodedContent = contentDecoder.getContent().content;
-				decodedContent.should.equal(str);
-			});
+      it('when adding text to the middle', function () {
+        str = str.splice(28, 0, 'Oh, here we are... ');
+        var decodedContent;
+        contentEncoder.push(str);
+        decodedContent = contentDecoder.getContent().content;
+        decodedContent.should.equal(str);
+      });
 
-			it('when removing text from the end', function () {
-				str = str.slice(0, -21);
-				var decodedContent;
-				contentEncoder.push(str);
-				decodedContent = contentDecoder.getContent().content;
-				decodedContent.should.equal(str);
-			});
+      it('when removing text from the end', function () {
+        str = str.slice(0, -21);
+        var decodedContent;
+        contentEncoder.push(str);
+        decodedContent = contentDecoder.getContent().content;
+        decodedContent.should.equal(str);
+      });
 
-			it('when removing text from the middle', function () {
-				str = str.splice(28, 19, '');
-				var decodedContent;
-				contentEncoder.push(str);
-				decodedContent = contentDecoder.getContent().content;
-				decodedContent.should.equal(str);
-			});
+      it('when removing text from the middle', function () {
+        str = str.splice(28, 19, '');
+        var decodedContent;
+        contentEncoder.push(str);
+        decodedContent = contentDecoder.getContent().content;
+        decodedContent.should.equal(str);
+      });
 
+      window.contentEncoder = contentEncoder;
+      window.contentDecoder = contentDecoder;
 
+    });
 
-		});
-
-	});
+  });
 })();
