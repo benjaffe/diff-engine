b7192d9f0d1e54838835fbbfeed2e6423433c8bb
Ben Jaffe <ben@udacity.com>
2015-04-10 14:24:56 -0700

    added timestamp checking

--- test/spec/test.js
+++ test/spec/test.js
@@ -17,14 +17,15 @@
       	// content: '<TextView\n    android:text=\"Hi there!\"\n    android:padding=\"20dp\"/>'
       	content: 'Bink'
       };
-      var decodedState;
+      var decodedState, previousTime;
 
       it('for adding the initial string to the `content` key', function () {
         stateEncoder.push(state);
 
-        decodedState = stateDecoder.getState().state;
-        console.log(decodedState);
-        decodedState.content.should.equal(state.content);
+        decodedState = stateDecoder.getState();
+        decodedState.state.content.should.equal(state.content);
+        decodedState.timestamp.should.equal(0);
+        previousTime = decodedState.timestamp;
       });
 
       it('when adding text to the end to the `content` key', function () {
@@ -32,48 +33,60 @@
         stateEncoder.push(state);
         console.log('pushing', state);
 
-        decodedState = stateDecoder.getState().state;
-        decodedState.content.should.equal(state.content);
+        decodedState = stateDecoder.getState();
+        decodedState.state.content.should.equal(state.content);
+        decodedState.timestamp.should.be.above(previousTime);
+        previousTime = decodedState.timestamp;
       });
 
       it('when adding text to the middle to the `content` key', function () {
         state.content = state.content.splice(8, 0, ' Oh, here we are ...');
         stateEncoder.push(state);
 
-        decodedState = stateDecoder.getState().state;
-        decodedState.content.should.equal(state.content);
+        decodedState = stateDecoder.getState();
+        decodedState.state.content.should.equal(state.content);
+        decodedState.timestamp.should.be.above(previousTime);
+        previousTime = decodedState.timestamp;
       });
 
       it('when removing text from the end to the `content` key', function () {
         state.content = state.content.slice(0, -21);
         stateEncoder.push(state);
 
-        decodedState = stateDecoder.getState().state;
-        decodedState.content.should.equal(state.content);
+        decodedState = stateDecoder.getState();
+        decodedState.state.content.should.equal(state.content);
+        decodedState.timestamp.should.be.above(previousTime);
+        previousTime = decodedState.timestamp;
       });
 
       it('when removing text from the middle to the `content` key', function () {
         state.content = state.content.splice(8, 19, '');
         stateEncoder.push(state);
 
-        decodedState = stateDecoder.getState().state;
-        decodedState.content.should.equal(state.content);
+        decodedState = stateDecoder.getState();
+        decodedState.state.content.should.equal(state.content);
+        decodedState.timestamp.should.be.above(previousTime);
+        previousTime = decodedState.timestamp;
       });
 
       it('when adding a mouse position key', function () {
         state.mouse = [152, 288];
         stateEncoder.push(state);
 
-        decodedState = stateDecoder.getState().state;
-        decodedState.content.should.equal(state.content);
+        decodedState = stateDecoder.getState();
+        decodedState.state.content.should.equal(state.content);
+        decodedState.timestamp.should.be.above(previousTime);
+        previousTime = decodedState.timestamp;
       });
 
       it('when changing a mouse position key', function () {
         state.mouse = [155, 288];
         stateEncoder.push(state);
 
-        decodedState = stateDecoder.getState().state;
-        decodedState.content.should.equal(state.content);
+        decodedState = stateDecoder.getState();
+        decodedState.state.content.should.equal(state.content);
+        decodedState.timestamp.should.be.above(previousTime);
+        previousTime = decodedState.timestamp;
       });
 
       window.stateEncoder = stateEncoder;
